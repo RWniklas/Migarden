b80ad20e5db349ba98fd2f280b66879e2862065d
RWniklas <[nicki.sissigmx.de]>
2025-03-28 18:48:58 +0100

    nIKLAS

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,24 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 import React from 'react';
-import header './components/header.jsx';
+import Header from './components/data/Header';
+import Hero from './components/data/Hero';
+import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Counter: {count}</h1>
-      <header label="Increment" onClick={handleClick} />
-    </div>
+    <>
+      <Header />
+     
+    </>
   );
-};
+}
 
 
 
