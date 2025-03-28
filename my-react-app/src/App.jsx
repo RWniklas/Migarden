3a126659fc703de67e8d7ce4cad03681c858e195
RWniklas <[nicki.sissigmx.de]>
2025-03-28 19:00:28 +0100

    Test3

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,9 +3,6 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 import React from 'react';
 import Header from './components/data/Header';
